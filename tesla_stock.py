d316fe9507acd083dc9e92438ce86f8c71989dfe
nicat-rzayev-aze <nijat.rzayev.aze@gmail.com>
2026-02-22 00:18:29 +0400

    Update and rename Project1.py to tesla_stock.py

--- tesla_stock.py
+++ tesla_stock.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup        #I imported the libraries 
 import requests as rq
 from sqlite3.dbapi2 import Date
-
-
 import yfinance as yf
+
+
 tesla = yf.Ticker("TSLA")
-tesla_data = tesla.history(period = "max")
+tesla_data = tesla.history(period = "max")    #I pulled historical data with yfinance
 tesla_data.reset_index(inplace=True)
 
 
 def make_graph(stock_data, revenue_data, stock):
-    stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
+    stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']        #This function is for making graph
     revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
 
     fig, axes = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
@@ -32,14 +32,14 @@ def make_graph(stock_data, revenue_data, stock):
     plt.show()
 
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
-web = rq.get(url)
-html_data = web.text
-soup = BeautifulSoup(html_data, "html.parser")
+web = rq.get(url)            #This link is course website of IBM. There are revenue history and growth rate of Tesla from 2010 to 2022.
+html_data = web.text                            
+soup = BeautifulSoup(html_data, "html.parser")  #I got HTML text with requests and I parsed this data with BeautifulSoup
 table = soup.find("table")
 rows = []
 for tr in table.find_all("tr")[1:]:
     cols = [td.get_text(strip=True) for td in tr.find_all("td")]
-    rows.append(cols)
+    rows.append(cols)                                                #I created a table with pandas. Also I cleaned them.
 tesla_revenue = pd.DataFrame(rows, columns=["Date", "Revenue"])
 tesla_revenue["Revenue"] = (
     tesla_revenue["Revenue"]
@@ -49,5 +49,6 @@ tesla_revenue["Revenue"] = (
 
 tesla_revenue["Revenue"] = pd.to_numeric(tesla_revenue["Revenue"], errors="coerce")
 tesla_revenue.dropna(inplace=True)
-tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]                #Last part. I created a graph using the function I created.
 make_graph(tesla_data, tesla_revenue, 'Tesla')
+
